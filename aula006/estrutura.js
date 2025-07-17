44081e84fbd96547a64dd15115796d140f161f5e
Eduard0Souza <vesouza1985@gmail.com>
2025-07-17 10:06:24 -0400

    estrutura de repetição

---
+++ aula006/estrutura.js
@@ -0,0 +1,14 @@
+/*var c = 1;
+while (c <= 6){
+    console.log(`${c}º Olá`);
+    c++;
+}
+var d = 1;
+do {
+    console.log(`${d}º Olá`);
+    d++;
+}while (d <= 6);*/
+//console.log('Fim do loop');
+for (var e = 1; e <= 6; e++){
+    console.log(`${e}º Olá`);
+}
